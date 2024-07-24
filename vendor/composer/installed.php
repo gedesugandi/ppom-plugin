193e48e72a4a1d0fb18ac3606ad8875b9af215de
gedesugandi <gedesugandi@gmail.com>
2024-07-24 11:01:37 +0800

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeinwp/woocommerce-product-addon',
+        'pretty_version' => 'v32.0.25',
+        'version' => '32.0.25.0',
+        'reference' => 'e9873359e8e9cd1a7f697958206c88910886548f',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => '3.3.25',
+            'version' => '3.3.25.0',
+            'reference' => '29b7c81f8ccd039f49d62ef7427a4cc06369becc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeinwp/woocommerce-product-addon' => array(
+            'pretty_version' => 'v32.0.25',
+            'version' => '32.0.25.0',
+            'reference' => 'e9873359e8e9cd1a7f697958206c88910886548f',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
